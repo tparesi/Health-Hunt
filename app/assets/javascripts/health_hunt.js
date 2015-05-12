e9e4dfd21ccb7406002402ecc776776cdf21d75a
Thomas Paresi <tparesi@gmail.com>
2015-05-12 14:39:54 -0400

    Switch root page to static_pages/root with backbone initialized

---
+++ app/assets/javascripts/health_hunt.js
@@ -0,0 +1,13 @@
+window.HealthHunt = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  ProductHuntClone.initialize();
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "sessions#new"
+  root to: 'static_pages#root'
 
   resources :users, only: [:create, :new]
   resource :session, only: [:create, :new, :destroy]
