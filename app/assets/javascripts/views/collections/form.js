2ddca72c495d5c41330df50c1d07bb1104067271
tparesi <tparesi@gmail.com>
2015-05-19 09:53:56 -0400

    Preview Picture on Collection Edit

--- app/assets/javascripts/views/collections/form.js
+++ app/assets/javascripts/views/collections/form.js
@@ -36,23 +36,25 @@ HealthHunt.Views.CollectionForm = Backbone.View.extend({
   },
 
   fileInputChange: function(event){
-    console.log(event.currentTarget.files[0]);
-
     var that = this;
     var file = event.currentTarget.files[0];
     var reader = new FileReader();
 
     reader.onloadend = function(){
+      that._updatePreview(reader.result);
       that.model._image = reader.result;
-      console.log(that.model);
     };
 
     if (file) {
       reader.readAsDataURL(file);
     } else {
+      that._updatePreview("");
       delete that.model._image;
-      console.log(that.model);
     }
+  },
+
+  _updatePreview: function(src){
+    this.$el.prepend("<img src='" + src + "'>");
   },
 
   deleteCollection: function (event) {
