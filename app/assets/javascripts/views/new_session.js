7b7d9a1fd23bd7c64b2ba86df9f6eea45bb3f692
tparesi <tparesi@gmail.com>
2015-05-29 18:26:52 -0400

    Fix back browsing from log in

--- app/assets/javascripts/views/new_session.js
+++ app/assets/javascripts/views/new_session.js
@@ -10,6 +10,15 @@ HealthHunt.Views.newSession = Backbone.View.extend({
     var authToken = $('meta[name=csrf-token]').attr('content');
     var content = this.template({authToken: authToken});
     this.$el.html(content);
+
+    $(window).on("hashchange", function(event) {
+      if (!window.location.hash.match(/#\/session*/)) {
+        $(".header").addClass("orange");
+        $(".subheader").css("display", "block");
+        this.remove();
+      }
+    }.bind(this));
+
     return this;
   }
 });
